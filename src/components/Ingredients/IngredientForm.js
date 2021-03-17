1411c55c0db6f4b518a559c1bf9df9e34143ede1
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2021-03-17 10:54:20 -0700

    added useeffect and firebase

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,12 +1,34 @@
-import React, { memo } from 'react'
+import React, { memo, useState } from 'react'
 
 import Card from '../UI/Card'
 import './IngredientForm.css'
 
 const IngredientForm = (props) => {
+
+
+  // const inputState = useState({
+  //   title: '',
+  //   amount: ''
+  // })
+
+  // const [ inputState, setInputState ] = useState({
+  //   title: '',
+  //   amount: ''
+  // })
+
+  const [enterTitle, setEnterTitle] = useState('')
+  const [enterAmount, setEnterAmount] = useState('')
+
+
   const submitHandler = (event) => {
     event.preventDefault()
     // ...
+    // if(!enterTitle && !enterAmount){
+    //   return alert('Enter the Value!!')
+    // }else{
+      
+    // }
+    props.onAddIngredient({title: enterTitle, amount: enterAmount})
   }
 
   return (
@@ -15,11 +37,19 @@ const IngredientForm = (props) => {
         <form onSubmit={submitHandler}>
           <div className='form-control'>
             <label htmlFor='title'>Name</label>
-            <input type='text' id='title' />
+            <input 
+              type='text' 
+              id='title' 
+              value={enterTitle} 
+              onChange={(event)=>setEnterTitle(event.target.value)} 
+            />
           </div>
           <div className='form-control'>
             <label htmlFor='amount'>Amount</label>
-            <input type='number' id='amount' />
+            <input type='number' id='amount' 
+              value={enterAmount} 
+              onChange={(event)=>setEnterAmount(event.target.value)}
+            />
           </div>
           <div className='ingredient-form__actions'>
             <button type='submit'>Add Ingredient</button>
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,58 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList'
 import Search from './Search';
 
 function Ingredients() {
+
+  const [userIngredients, setUserIngredients] = useState([])
+
+  useEffect(() => {
+    fetch('https://react-hooks-update-b4d15-default-rtdb.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = []
+        for(const key in responseData){
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          })
+        }
+        setUserIngredients(loadedIngredients)
+      })
+  }, [])
+
+  const addIngredientsHandler = (ingredients) => {
+    // setUserIngredients((prevState)=>[
+    //   ...prevState, 
+    //   {id: Math.random().toString(), ...ingredients}
+    // ])
+
+    fetch('https://react-hooks-update-b4d15-default-rtdb.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredients),
+      headers: {'Content-Type': 'application/json'}
+    })
+      .then(response => response.json())
+      .then(responseData => {
+         setUserIngredients((prevState)=>[
+          ...prevState, 
+          {id: responseData.name, ...ingredients}
+        ])
+      })
+
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientsHandler} />
 
       <section>
         <Search />
         {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
       </section>
     </div>
   );
